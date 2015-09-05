ff9ef3c4f01bdd4ebc4afac79015bfa44e45a64d
Tassio <tabm2010@gmail.com>
2015-09-05 11:52:44 -0300

    Atualização do trabalho 2.
    O módulo dado completo.
    
    O módulo dado pontos  com apenas uma função criada, criadadoponto.
    
    PDF do modelo do programa, dá pra modificar no projeto do processon

---
+++ DADOPONTOS.h
@@ -0,0 +1,119 @@
+/***************************************************************************
+*
+*  $MCD Módulo de definição: Módulo Dado
+*
+*  Arquivo gerado:              DadoPontos.H
+*  Letras identificadoras:      DADPnt
+*
+*  Nome da base de software:    Exemplo de teste automatizado
+*
+*  Projeto: Disciplinas INF 1628 / 1301
+*  Gestor:  DI/PUC-Rio
+*  Autores: avs - Arndt von Staa
+*			fvc - Felipe Vieira Côrtes
+*			tbm - Tássio Borges de Miranda
+*			db  - Daniela Brazão
+
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*       1.00   tbm   05/09/2015 Início do desenvolvimento
+*
+*  $ED Descrição do módulo
+*     Este módulo implementa um conjunto simples de funções para verificar o
+*	  valor atual da partida e qual jogador pode utilizar o dado.
+*
+***************************************************************************/
+ 
+#if defined( DADOPONTOS_OWN )
+   #define DADOPONTOS_EXT
+#else
+   #define DADOPONTOS_EXT extern
+#endif
+
+#include "LISTA.H"
+
+typedef struct tgDadoPontos * tppDadoPontos;
+/***********************************************************************
+*
+*  $TC Tipo de dados: MAT Condicoes de retorno
+*
+*
+***********************************************************************/
+
+   typedef enum {
+
+         DADPnt_CondRetOK = 0 ,
+               /* Executou correto */
+
+         DADPnt_CondRetErro = 1 ,
+               /* Estrutura do DadoPontos está errada */
+
+         DADPnt_CondRetMatrizNaoExiste = 2 ,
+               /* DadoPontos não existe */
+
+         DADPnt_CondRetFaltouMemoria = 3
+               /* Faltou memória ao alocar dados */
+
+   } DADPnt_tpCondRet ;
+
+
+/***********************************************************************
+*
+*  $FC Função: DADPnt Criar Dado de Pontos
+*
+*  $ED Descrição da função
+*	  Cria um novo dado de pontos
+*  $FV Valor retonado
+*     DADPnt_CondRetOK
+*     DADPnt_CondRetFaltouMemoria
+*
+***********************************************************************/
+
+DADPnt_tpCondRet DADPtn_CriarDado(tppDadoPontos * Dado)
+
+/***************************************************************************
+*
+*  $FC Função: DADPnt Dobrar Dado de Pontos
+*
+*  $ED Descrição da função
+*	  Dobra o valor da partida
+*  $FV Valor retonado
+*     DADPnt_CondRetOK
+*     DADPnt_CondRetFaltouMemoria
+*
+***********************************************************************/
+
+DADPnt_tpCondRet DADPtn_DobrarDado(tppDadoPontos Dado);
+
+/***************************************************************************
+*
+*  $FC Função: DADPnt Mudar "Dono" do Dado de Pontos
+*
+*  $ED Descrição da função
+*	  Muda o jogador que possa usar o Dado de Pontos
+*  $FV Valor retonado
+*     DADPnt_CondRetOK
+*     DADPnt_CondRetFaltouMemoria
+*
+***********************************************************************/
+
+DADPnt_tpCondRet DADPtn_MudDono(tppDadoPontos Dado);
+
+/***************************************************************************
+*
+*  $FC Função: DADPnt Destroi o Dado de Pontos
+*
+*  $ED Descrição da função
+*	  Destroi o Dado de Pontos
+*  $FV Valor retonado
+*     DADPnt_CondRetOK
+*     DADPnt_CondRetFaltouMemoria
+*
+***********************************************************************/
+
+DADPnt_tpCondRet DADPtn_DestruirDado(tppDadoPontos Dado);
+
+#undef DADOPONTOS_EXT
+
+/*************** Fim do módulo de definição: Módulo DadoPontos ****************/
